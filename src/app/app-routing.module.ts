077ff356e95e7a00bb71229e5021dde4bf62ebe8
Martin Pritchard <martinpritchard@elevatedirect.com>
2019-09-18 10:18:40 +0100

    Add Mobile and Desktop Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MasterComponent } from './pages/master/master.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
+const detailRoutes = [
+  {
+    path: 'detail',
+    component: DetailComponent
+  }
+];
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'master',
+    pathMatch: 'full'
+  },
+  {
+    path: 'master',
+    component: MasterComponent,
+    children: [
+      ...detailRoutes
+    ]
+  },
+  ...detailRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
